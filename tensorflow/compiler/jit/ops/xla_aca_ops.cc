8df237ea93586a32cc82a11da7ce51cfb25c08a0
el3ctrician <elbadriahmad@gmail.com>
2018-09-03 02:10:27 +0200

    switched from normal op to xla op,to be implemented

--- tensorflow/compiler/jit/ops/xla_aca_ops.cc
+++ tensorflow/compiler/jit/ops/xla_aca_ops.cc
@@ -6,51 +6,70 @@
  * output = input1 + (input2 * input3)
  **/
 
-#include "tensorflow/core/framework/op.h"
-#include "tensorflow/core/framework/shape_inference.h"
-#include "tensorflow/core/framework/op_kernel.h"
+
+/**
+ * The following code is for a normal Tensorflow op and rappresents a high level implementation
+ * XLA code is slightly diffrent and Ops can be found in tensorflow/compiler/tf2xla/kernels
+ * this is why this code will be moved there and ops will be implemented accordingly
+ * Code is left here for reference 
+ *  */
+/** 
+ * NB : add this to BUILD to build this file
+ *                   |
+ *                   |
+ *                   v
+ * cc_library(
+    name = "xla_aca_ops",
+    srcs = ["xla_aca_ops.cc"],
+)
+**/
 
 
-using namespace tensorflow;
-
-REGISTER_OP("FusedAcaAddMul")
-    .Input("addVar: int32")
-    .Input("mulVar1: int32")
-    .Input("mulVar2: int32")
-    .Output("result: int32")
-    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-      c->set_output(0, c->input(0));
-      return Status::OK();
-    });
+// #include "tensorflow/core/framework/op.h"
+// #include "tensorflow/core/framework/shape_inference.h"
+// #include "tensorflow/core/framework/op_kernel.h"
 
 
-class FusedAcaAddMulOp : public OpKernel {
- public:
-  explicit FusedAcaAddMulOp(OpKernelConstruction* context) : OpKernel(context) {}
+// using namespace tensorflow;
 
-  void Compute(OpKernelContext* context) override {
-    // Grab the input tensor
-    const Tensor& input_add_tensor = context->input(0);
-    const Tensor& input_mul1_tensor = context->input(1);
-    const Tensor& input_mul2_tensor = context->input(2);
-    auto input_add = input_add_tensor.flat<int32>();
-    auto input_mul1 = input_mul1_tensor.flat<int32>();
-    auto input_mul2 = input_mul2_tensor.flat<int32>();
+// REGISTER_OP("FusedAcaAddMul")
+//     .Input("addVar: int32")
+//     .Input("mulVar1: int32")
+//     .Input("mulVar2: int32")
+//     .Output("result: int32")
+//     .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+//       c->set_output(0, c->input(0));
+//       return Status::OK();
+//     });
 
-    // Create an output tensor
-    Tensor* output_tensor = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
-                                                     &output_tensor));
-    auto output_flat = output_tensor->flat<int32>();
 
-    // Set all but the first element of the output tensor to 0.
-    const int N = input.size();
-    for (int i = 1; i < N; i++) {
-      output_flat(i) = input_add(i) + (input_mul1(i) * input_mul2(i));
-    }
-  }
-};
+// class FusedAcaAddMulOp : public XlaOpKernel {
+//  public:
+//   explicit FusedAcaAddMulOp(OpKernelConstruction* context) : OpKernel(context) {}
 
-REGISTER_KERNEL_BUILDER(Name("FusedAcaAddMul").Device(DEVICE_CPU), FusedAcaAddMulOp);
+//   void Compute(OpKernelContext* context) override {
+//     // Grab the input tensor
+//     const Tensor& input_add_tensor = context->input(0);
+//     const Tensor& input_mul1_tensor = context->input(1);
+//     const Tensor& input_mul2_tensor = context->input(2);
+//     auto input_add = input_add_tensor.flat<int32>();
+//     auto input_mul1 = input_mul1_tensor.flat<int32>();
+//     auto input_mul2 = input_mul2_tensor.flat<int32>();
+
+//     // Create an output tensor
+//     Tensor* output_tensor = NULL;
+//     OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
+//                                                      &output_tensor));
+//     auto output_flat = output_tensor->flat<int32>();
+
+//     // Set all but the first element of the output tensor to 0.
+//     const int N = input.size();
+//     for (int i = 1; i < N; i++) {
+//       output_flat(i) = input_add(i) + (input_mul1(i) * input_mul2(i));
+//     }
+//   }
+// };
+// //lunch the kernel on XLA device
+// REGISTER_XLA_OP(Name("FusedAcaAddMul").Device(DEVICE_XLA_CPU_JIT), FusedAcaAddMulOp);
 
 
---
+++ tensorflow/compiler/tf2xla/kernels/ACA_LinearEqOp.cc
@@ -0,0 +1,41 @@
+/**
+ * ACA Project
+ * Definition of a new OP that will the fusion of other two elementary ops
+ * 
+ * We are defining the fusion of "Add" and "Mul"
+ * output = input1 + (input2 * input3) //can also be interpeted as a linear equation Y = (m*X) + b
+ **/
+
+
+#include "tensorflow/compiler/tf2xla/xla_op_kernel.h"
+#include "tensorflow/compiler/tf2xla/xla_op_registry.h"
+
+namespace tensorflow {
+namespace {
+
+class LinearEqOp : public XlaOpKernel {
+ public:
+  explicit LinearEqOp(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {}
+
+  void Compile(XlaOpKernelContext* ctx) override {
+    if (!ctx->ValidateInputsAreSameShape(this)) return;
+
+    OP_REQUIRES(ctx, ctx->num_inputs() >= 1,
+                errors::InvalidArgument("AddN requires at least one argument"));
+
+    xla::ComputationDataHandle sum = ctx->Input(0);
+    for (int i = 1; i < ctx->num_inputs(); ++i) {
+      sum = ctx->builder()->Add(sum, ctx->Input(i));
+    }
+
+    ctx->SetOutput(0, sum);
+  }
+
+ private:
+  TF_DISALLOW_COPY_AND_ASSIGN(LinearEqOp);
+};
+
+REGISTER_XLA_OP(Name("LinearEq"), LinearEqOp);
+
+}  // namespace
+}  // namespace tensorflow
