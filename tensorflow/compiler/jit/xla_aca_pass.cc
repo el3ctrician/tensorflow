61bc6a4f4e3e260cb2aafdb457a622d954550027
thestarivore <eduardionut.chirica@mail.polimi.it>
2018-09-05 22:23:45 +0200

    -Test4

--- tensorflow/compiler/jit/xla_aca_pass.cc
+++ tensorflow/compiler/jit/xla_aca_pass.cc
@@ -74,7 +74,7 @@ namespace tensorflow {
                 //subedges[j++] = subedge;
 
                 //Connect the inputs of the MatMul operation to the new operation
-                String tmp = new_node->type_string();
+                string tmp = new_node->type_string();
                 graph_out->AddEdge(subedge->src(), subedge->dst_input(), new_node, i++);
               }
 
