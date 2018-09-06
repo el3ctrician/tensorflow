3250601c55821365092e0725483b65315b8a3115
thestarivore <eduardionut.chirica@mail.polimi.it>
2018-09-06 23:18:23 +0200

    - Added filter if no add-mul op founded

--- tensorflow/compiler/jit/xla_aca_pass.cc
+++ tensorflow/compiler/jit/xla_aca_pass.cc
@@ -42,6 +42,7 @@ namespace tensorflow {
     const Edge* edges[10];
     const Edge* subedges[10]; //store all subedges of the edges of the Add operation
     Node* add_node;     //store the add node
+    boolean found_addmulops = false;
 
     // Loop through our graph nodes !.
     for (Node* n : graph_out->op_nodes()) {
@@ -65,6 +66,7 @@ namespace tensorflow {
           if(edge->src()->type_string() == "MatMul"){
               VLOG(1) << "      ACA_Project : -------------------------Node Input Edge of an Edge Analysis---------------------------";
 
+              found_addmulops = true;
               int j=0;
               // Loop through the input edges of the edges
               for (const Edge* subedge : edge->src()->in_edges()){
@@ -103,28 +105,28 @@ namespace tensorflow {
     VLOG(1) << "ACA_Project : -----------------------------END---------------------------------";
 
 
+    if(found_addmulops){
+      //New node creation
+      Status status;
+      NodeDef node_def;
+      //node_def.set_name(graph_out->NewName("LinearEqOp"));
+      node_def.set_op("LinearEq");
+      //AddNodeAttr( "LinearEq", 0, &node_def);
+      Node* new_node = graph_out->AddNode(node_def, &status);
+      string tmp = new_node->type_string();
 
-    //New node creation
-    Status status;
-    NodeDef node_def;
-    //node_def.set_name(graph_out->NewName("LinearEqOp"));
-    node_def.set_op("LinearEq");
-    //AddNodeAttr( "LinearEq", 0, &node_def);
-    Node* new_node = graph_out->AddNode(node_def, &status);
-    string tmp = new_node->type_string();
 
+      //Modify the graph
+      //Connect the inputs of the MatMul operation to the new operation
+      graph_out->AddEdge(subedges[0]->src(), subedges[0]->dst_input(), new_node, 0);
+      graph_out->AddEdge(subedges[1]->src(), subedges[1]->dst_input(), new_node, 1);
+      //Bisogna aggiungere il secondo input del nodo principale
+      graph_out->AddEdge(edges[1]->src(), edges[1]->dst_input(), new_node, 2);
 
-    //Modify the graph
-    //Connect the inputs of the MatMul operation to the new operation
-    graph_out->AddEdge(subedges[0]->src(), subedges[0]->dst_input(), new_node, 0);
-    graph_out->AddEdge(subedges[1]->src(), subedges[1]->dst_input(), new_node, 1);
-    //Bisogna aggiungere il secondo input del nodo principale
-    graph_out->AddEdge(edges[1]->src(), edges[1]->dst_input(), new_node, 2);
-
-    //remove node and edge after setted up the new node    
-    graph_out->RemoveEdge(edges[0]);    //remove MatMul node
-    graph_out->RemoveNode(add_node);    //remove Add node
-
+      //remove node and edge after setted up the new node    
+      graph_out->RemoveEdge(edges[0]);    //remove MatMul node
+      graph_out->RemoveNode(add_node);    //remove Add node
+    }
 
 
     //Print again everything so that we can verify
