697e49dcc281a203db8632c56cb1b590b7a51d90
thestarivore <eduardionut.chirica@mail.polimi.it>
2018-09-09 18:34:28 +0200

    - Changed LinearEq in core/kernels 11

--- tensorflow/core/kernels/xla_aca_ops.cc
+++ tensorflow/core/kernels/xla_aca_ops.cc
@@ -64,7 +64,7 @@ class LinearEqOp : public OpKernel {
     auto output_flat = output_tensor->flat<int32>();
 
     // Set all but the first element of the output tensor to 0.
-    output_tensor = &input_mul1_tensor;
+    output_tensor = context->builder()->Add(output_tensor, input_mul1_tensor);
     output_tensor = context->builder()->Mul(output_tensor, input_mul2_tensor);
     output_tensor = context->builder()->Add(output_tensor, input_add_tensor);
 
