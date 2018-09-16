c38696132cf1da90431d98a7f976f9e9283932d7
thestarivore <eduardionut.chirica@mail.polimi.it>
2018-09-16 13:51:09 +0200

    - Try to adapt matrixes that are of different sizes 12

--- tensorflow/core/kernels/xla_aca_ops.cc
+++ tensorflow/core/kernels/xla_aca_ops.cc
@@ -117,7 +117,7 @@ class LinearEqOp : public OpKernel {
   bool transpose_a_;
   bool transpose_b_;
 };
-REGISTER_KERNEL_BUILDER(Name("LinearEq").Device(DEVICE_CPU), LinearEqOp);
+REGISTER_KERNEL_BUILDER(Name("LinearEq").Device(DEVICE_CPU), LinearEqOp<CPUDevice, float, false /* cublas, ignored for CPU */>);
 
 
 
